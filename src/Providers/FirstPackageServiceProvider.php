aedcfe943d76bebd9138b136abc35f6571b46c14
Carlos A. Guevara <solrak.m@gmail.com>
2020-06-11 19:52:05 -0500

    publish views

--- src/Providers/FirstPackageServiceProvider.php
+++ src/Providers/FirstPackageServiceProvider.php
@@ -9,8 +9,10 @@ class FirstPackageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-       // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views/','first-package');
+        // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views/', 'first-package');
+
+        $this->publishes([__DIR__ . '/../../resources/views/' => resource_path('views/vendor/first-package')],'first-package-views');
     }
 
     public function register()
