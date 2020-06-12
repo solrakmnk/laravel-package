372cd1cac7dcecc9ab848eadda02e112aa7348fa
Carlos A. Guevara <solrak.m@gmail.com>
2020-06-12 12:26:43 -0500

    Archivos de configuracion

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 //Route::get('hello-route','Solrakmnk\FirstPackage\Http\Controllers\HelloController');
-Route::get('hello-route','HelloController');
+Route::get(config('first-package.route'),'HelloController');
 
 ?>
--- src/Providers/FirstPackageServiceProvider.php
+++ src/Providers/FirstPackageServiceProvider.php
@@ -10,9 +10,13 @@ class FirstPackageServiceProvider extends ServiceProvider
     public function boot()
     {
         // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views/', 'first-package');
+        $this->loadViewsFrom($this->basePath('resources/views/'), 'first-package');
 
-        $this->publishes([__DIR__ . '/../../resources/views/' => resource_path('views/vendor/first-package')],'first-package-views');
+        $this->publishes([$this->basePath('resources/views/') => resource_path('views/vendor/first-package')],
+            'first-package-views');
+
+        $this->publishes([$this->basePath('config/first-package.php') => resource_path('config/first-package.php')],
+            'first-package-config');
     }
 
     public function register()
@@ -20,6 +24,13 @@ class FirstPackageServiceProvider extends ServiceProvider
         $this->app->bind('first-package', function () {
             return new Hello;
         });
+
+        $this->mergeConfigFrom($this->basePath('config/first-package.php'), 'first-package');
+    }
+
+    protected function basePath($path = '')
+    {
+        return __DIR__ . '/../../' . $path;
     }
 
 }
